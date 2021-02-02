7ba9cd426d914ff4061baab91ef969fa2cfbef68
Chen Gong <chengong99@berkeley.edu>
2021-02-02 10:31:44 -0500

    implement of option_b

--- PPM_Image_Transformtion/pbm_aux.c
+++ PPM_Image_Transformtion/pbm_aux.c
@@ -1,11 +1,32 @@
 #include "pbm.h"
+#include <stdlib.h>
 
 PPMImage * new_ppmimage( unsigned int w, unsigned int h, unsigned int m )
 {
+    PPMImage* imageIn;
+    imageIn = malloc(sizeof(PPMImage));
+    imageIn -> width = w;
+    imageIn -> height = h;
+    imageIn -> max = m;
+    for (int i = 0; i < 3; i++){
+        imageIn -> pixmap[i] = malloc(h * sizeof(int*));
+        for (int j = 0; j < h; j++) {
+            imageIn -> pixmap[i][j] = malloc(w * sizeof(int));
+        }
+    }
+    return imageIn;    //question: would the struct be destoryed after this function;
 }
 
 PBMImage * new_pbmimage( unsigned int w, unsigned int h )
 {
+    PBMImage* image = malloc(sizeof(PBMImage));
+    image -> width = w;
+    image -> height = h;
+    image -> pixmap = malloc(h * sizeof(int*));
+    for(int i = 0; i < h; i++){
+        image -> pixmap[i] = malloc(w * sizeof(int));
+    }
+    return image;
 }
 
 PGMImage * new_pgmimage( unsigned int w, unsigned int h, unsigned int m )
--- PPM_Image_Transformtion/ppmcvt.c
+++ PPM_Image_Transformtion/ppmcvt.c
@@ -3,15 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void option_b();
-void option_g();
-void option_i();
-void option_r();
-void option_s();
-void option_m();
-void option_t();
-void opiton_n();
-void option_o();
 
 typedef struct
 {
@@ -22,59 +13,119 @@ typedef struct
     
 }Option;
 
-Option read_options(int arc, char*argv[]);    //return the option, and handle the exceptions, assign file names;
+void option_b(Option op, PPMImage* image_in); //convert input file to a Portable Bitmap (PBM) file
+void option_g(Option op, PPMImage* image_in);//convert input file to a Portable Gray Map (PGM) file using the specified max grayscale pixel value [1-65535].
+void option_i();//isolate the specified RGB channel. Valid channels are “red”, “green”, or “blue”.
+void option_r();//remove the specified RGB channel. Valid channels are “red”, “green”, or “blue”.
+void option_s();
+void option_m();
+void option_t();
+void opiton_n();
+void option_o();
 
+
+Option read_options(int arc, char*argv[]);//return the option, and handle the exceptions, assign file names;
+
+void image_transform (Option op, PPMImage image_in);
 
 
 int main( int argc, char *argv[] )
 {
     Option option = read_options(argc, argv);
     
+    printf("infilename in main:%s\n", option.infile_name);
+    PPMImage* image_in = read_ppmfile(option.infile_name);
+    
+    //do transformation base on option
+    switch(option.mode) {     // finish all the rest things here?
+        case 'b':
+            option_b(option, image_in);
+            break;
+        case 'g':
+            option_g(option, image_in);
+            break;
+        case 'i':
+            
+            break;
+        case 'r':
+            
+            break;
+        case 's':
+            
+            break;
+        case 'm':
+            
+            break;
+        case 't':
+           
+            break;
+        case 'n':
+            
+            break;
+    }
+    
+    
+    
+    
     
     return 0;
 }
+
+
 
 Option read_options(int argc, char*argv[]){
     int o_find = 0;
     int op_found = 0; //only accept one option except for -o?
     Option option;
+    
     int o;
+    
     while( (o=getopt(argc,argv,"bg:i:r:smt:n:o")) != -1){
-//        if(option_found){
-//            printf("too many options\n");
-//            exit();
-//        }
+        if(op_found){    // check if multiple options are given
+            fprintf(stderr, "Error: Multiple transformations specified\n");
+            exit(-1);
+        }
         switch (o) {
             case 'b':
+                
                 option.mode = 'b';
                 op_found = 1;
                 break;
             case 'g':
-               
+                
                 break;
             case 'i':
                 
                 break;
             case 'r':
+                
                 break;
             case 's':
+                
                 break;
             case 'm':
+                
                 break;
             case 't':
+               
                 break;
             case 'n':
+                
                 break;
             case 'o': // assign the output file.
                 o_find = 1;
+                
+                
                 if (!op_found)      // set default option b
                     option.mode = 'b';
-                if ((optind + 2) >= argc){
-                    printf("Output file and Input file required\n");
+                if ((optind + 2) > argc){
+                    fprintf(stderr, "Output file and Input file required\n");
                     exit(0);
                 }
-                option.outfile_name = argv[optind];
+                option.outfile_name = argv[optind];       //not necessarily follow o.
                 option.infile_name = argv[optind + 1];
+                printf("outfilename:%s\n", argv[optind]);
+                printf("inputfilename:%s\n", argv[optind+1]);
                 break;
             default:
                 printf("unexpected arg");
@@ -84,8 +135,69 @@ Option read_options(int argc, char*argv[]){
     
     if (o_find == 0) {
         printf("Expected an output opiton -o\n");
-        exit(0);
+        exit(-1);
     }
     
     return option;
+}
+
+
+void helper_check_PPMIImage(PPMImage* image_in){
+    int h = image_in -> height;
+    int w = image_in -> width;
+    int max = image_in -> max;
+    
+    printf("PPMMAX is %d", max);
+    float PPMMax = (float)image_in -> max;
+    float PPMMaxabovetwo = PPMMax/2;
+    printf("PPMMax/2 = %f\n", PPMMaxabovetwo);
+    
+    for (int color = 0; color < 3; color++){
+        for(int r = 0; r < h; r++){
+            for(int c = 0; c < w; c++){
+                printf(" %d", image_in -> pixmap[color][r][c]);
+            }
+            printf("\n");
+        }
+        printf("\n\n");
+    }
+    
+}
+
+void option_b(Option op, PPMImage* image_in){  //convert input file to a Portable Bitmap (PBM) file
+    //alloc new outfile
+    //transform
+    //write
+    PBMImage* image_out = new_pbmimage(image_in -> width, image_in -> height);
+    helper_check_PPMIImage(image_in);
+    //To compute black and white bits from RGB pixels use: 𝐴𝑣𝑒𝑟𝑎𝑔𝑒(𝑅 + 𝐺 + 𝐵) < 𝑃𝑃𝑀𝑀𝑎𝑥/2
+    for (int r = 0; r < image_in -> height; r++) {
+        for (int c = 0; c < image_in -> width; c++){
+            float RGB_sum = 0;
+            for (int color = 0; color < 3; color++){
+                RGB_sum = RGB_sum + (float)image_in -> pixmap[color][r][c];
+            }
+            float average_RGB = RGB_sum / 3;
+            float PPMMax = (float)image_in -> max;
+            float PPMMaxabovetwo = PPMMax/2;
+            printf("RGB/3 = %f\n", average_RGB);
+            if (average_RGB < PPMMax/2){
+                image_out -> pixmap[r][c] = 0;
+                printf("[%d][%d] is 0\n\n", r,c);
+            } else {
+                image_out -> pixmap[r][c] = 1;
+                printf("[%d][%d] is 1\n\n", r,c);
+            }
+        }
+    }
+    
+    write_pbmfile(image_out, op.outfile_name);
+    
+    
+}
+
+
+void option_g(Option op, PPMImage* image_in){
+    
+    
 }
